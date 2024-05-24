72b3338e0db22787dd87b1f165eea3ae2cf2c0e3
Zunay <sweetxdrops@gmail.com>
2024-05-24 02:07:14 -0300

    tp-front

---
+++ G8/data/articulos.js
@@ -0,0 +1,115 @@
+const articulos = [
+    {
+        "id":"H0001",
+        "nombre":"Short deportivo",
+        "categoria":"Pantalones",
+        "tipo":"Hombre",
+        "stock":[
+            {"talle":"XS", "disponible":5},
+            {"talle":"S", "disponible":10},
+            {"talle":"M", "disponible":12},
+            {"talle":"L", "disponible":8},
+            {"talle":"XL", "disponible":null},
+            {"talle":"XXL", "disponible":3}
+        ]   
+    },
+    {
+        "id":"H0002",
+        "nombre":"Camiseta básica",
+        "categoria":"Remeras",
+        "tipo":"Hombre",
+        "stock":[
+            {"talle":"XS", "disponible":5},
+            {"talle":"S", "disponible":10},
+            {"talle":"M", "disponible":12},
+            {"talle":"L", "disponible":8},
+            {"talle":"XL", "disponible":null},
+            {"talle":"XXL", "disponible":3}
+        ]   
+    },
+    {
+        "id":"M0001",
+        "nombre":"Top deportivo",
+        "categoria":"Remeras",
+        "tipo":"Mujer",
+        "stock":[
+            {"talle":"XS", "disponible":5},
+            {"talle":"S", "disponible":10},
+            {"talle":"M", "disponible":12},
+            {"talle":"L", "disponible":8},
+            {"talle":"XL", "disponible":null},
+            {"talle":"XXL", "disponible":3}
+        ]   
+    },
+    {
+        "id":"M0002",
+        "nombre":"Calzas básicas",
+        "categoria":"Pantalones",
+        "tipo":"Mujer",
+        "stock":[
+            {"talle":"XS", "disponible":5},
+            {"talle":"S", "disponible":10},
+            {"talle":"M", "disponible":12},
+            {"talle":"L", "disponible":8},
+            {"talle":"XL", "disponible":null},
+            {"talle":"XXL", "disponible":3}
+        ]   
+    },
+    {
+        "id":"N0001",
+        "nombre":"Remera básica",
+        "categoria":"Remeras",
+        "tipo":"Niño",
+        "stock":[
+            {"talle":"XS", "disponible":5},
+            {"talle":"S", "disponible":10},
+            {"talle":"M", "disponible":12},
+            {"talle":"L", "disponible":8},
+            {"talle":"XL", "disponible":null},
+            {"talle":"XXL", "disponible":3}
+        ]   
+    },
+    {
+        "id":"N0002",
+        "nombre":"Short deportivo",
+        "categoria":"Pantalones",
+        "tipo":"Niño",
+        "stock":[
+            {"talle":"XS", "disponible":5},
+            {"talle":"S", "disponible":10},
+            {"talle":"M", "disponible":12},
+            {"talle":"L", "disponible":8},
+            {"talle":"XL", "disponible":null},
+            {"talle":"XXL", "disponible":3}
+        ]   
+    },
+    {
+        "id":"M0003",
+        "nombre":"Buzo con capucha",
+        "categoria":"Abrigos",
+        "tipo":"Mujer",
+        "stock":[
+            {"talle":"XS", "disponible":5},
+            {"talle":"S", "disponible":10},
+            {"talle":"M", "disponible":12},
+            {"talle":"L", "disponible":8},
+            {"talle":"XL", "disponible":null},
+            {"talle":"XXL", "disponible":3}
+        ]   
+    },
+    {
+        "id":"M0004",
+        "nombre":"Campera",
+        "categoria":"Abrigos",
+        "tipo":"Mujer",
+        "stock":[
+            {"talle":"XS", "disponible":5},
+            {"talle":"S", "disponible":10},
+            {"talle":"M", "disponible":12},
+            {"talle":"L", "disponible":8},
+            {"talle":"XL", "disponible":null},
+            {"talle":"XXL", "disponible":3}
+        ]   
+    }
+    
+]
