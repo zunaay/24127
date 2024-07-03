2e617921da27ee79d0c386642e329593d44cc597
Zunay <sweetxdrops@gmail.com>
2024-07-03 19:11:59 -0300

    implementacion BACK/FRONT

--- BACK/data/db.js
+++ BACK/data/db.js
@@ -6,13 +6,4 @@ const db = new Sequelize("24127g8", "root", "", {
     port: 3306
 });
 
-// Alwaysdata
-
-// const db = new Sequelize("zunay_24127", "zunay", "24127_g8", {
-//     host: "mysql-zunay.alwaysdata.net",
-//     dialect: "mysql",
-//     port: 3306
-// });
-
-
 module.exports = db;
